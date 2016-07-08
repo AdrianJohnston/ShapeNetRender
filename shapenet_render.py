9b88c7650b559fd0b0b6bb11bc135252c4d619b1
adrian <Adrian.JohnstonAUS@gmail.com>
2016-07-08 13:22:34 +0930

    Changed shapenet_render

--- shapenet_render.py
+++ shapenet_render.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output_dir', default=output_dir,help='Model directory to load or save model_file from')
     parser.add_argument('-n', '--num_classes', default=55, type=int)
     parser.add_argument('-d', '--dry_run', default=True, action='store_true')
-    parser.add_argument('-j', '--num_processes', default=2)
+    parser.add_argument('-j', '--num_processes', default=2, type=int)
     args = parser.parse_args()
     print (args)
 
